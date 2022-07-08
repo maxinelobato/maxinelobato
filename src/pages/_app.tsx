a2bfe6bfec59765f7718901bfeb435f551d1a6a0
maxinelobato <maxinelobato89@hotmail.com>
2022-07-08 18:21:14 -0300

    changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,7 @@
-import {
-  ChakraProvider,
-  ColorModeScript,
-  extendTheme,
-  ThemeConfig,
-} from "@chakra-ui/react";
+import { ChakraProvider, extendTheme, ThemeConfig } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import GTag from "../components/Analytics/GTag";
 import { FramerMotionViewPort } from "../components/FramerMotionViewPort";
-import { useEffect } from "react";
-import Script from "next/script";
-import { useRouter } from "next/router";
-import * as fbq from "../../lib/fpixel";
-import * as gtag from "../../lib/gtag";
-// import { GTM_ID, pageview } from "../../lib/gtm";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
@@ -22,92 +12,12 @@ const theme = extendTheme({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  // useEffect(() => {
-  //   router.events.on("routeChangeComplete", pageview);
-  //   return () => {
-  //     router.events.off("routeChangeComplete", pageview);
-  //   };
-  // }, [router.events]);
-
-  useEffect(() => {
-    // This pageview only triggers the first time (it's important for Pixel to have real information)
-    fbq.pageview();
-
-    const handleRouteChange = (url) => {
-      fbq.pageview();
-      gtag.pageview(url);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
-    <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
-
-      {/* Global Site Code Pixel - Facebook Pixel */}
-      <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', ${fbq.FB_PIXEL_ID});
-          `,
-        }}
-      />
-
-      {/* Google Tag Manager - Global base code
-      <Script
-        id="gtag-base"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', '${GTM_ID}');
-          `,
-        }}
-      /> */}
-
-      <ChakraProvider theme={theme}>
-        <ColorModeScript type="cookie" />
-        <FramerMotionViewPort />
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </>
+    <ChakraProvider theme={theme}>
+      <GTag />
+      <FramerMotionViewPort />
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 
